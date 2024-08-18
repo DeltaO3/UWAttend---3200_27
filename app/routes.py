2aee3d38dae88ffa08c3f661a55b3d04dc848646
ozzy1323232 <168622926+ozzy1323232@users.noreply.github.com>
2024-08-18 13:18:18 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,34 @@
 from flask import render_template
 from app import app
 
-@app.route('/', methods=['GET'])
-@app.route('/index', methods=['GET'])
-def index():
-	return render_template('index.html')
+
+
+
+# HOME -   /home/
+@app.route('/home', methods=['GET'])
+def home():
+    return flask.render_template('1.html')
+
+# CONFIGURATION - /session/ /admin/
+@app.route('/session', methods=['GET'])
+def session():
+    return flask.render_template('1.html')
+
+@app.route('/admin', methods=['GET'])
+def admin():
+    return flask.render_template('1.html')
+
+# STUDENT - /student/
+@app.route('/student', methods=['GET'])
+def student():
+    return flask.render_template('1.html')
+	
+# LOGIN - /login/ 
+@app.route('/login', methods=['GET'])
+def login():
+    return flask.render_template('1.html')
+
+
+
+
+app.run(port=5958)
