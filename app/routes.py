c8254008dd8a9124de5003f36f29edaf209e61ba
Olivia Hanly (23331483) <23331483@student.uwa.edu.au>
2024-08-19 11:39:26 +0800

    Create login page and basic functionality

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 import flask
 from app import app
-
+from app.forms import LoginForm
 
 
 
@@ -25,6 +25,17 @@ def student():
     return flask.render_template('student.html')
 	
 # LOGIN - /login/ 
-@app.route('/login', methods=['GET'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return flask.render_template('login.html')
+
+    # placeholder values for testing
+    username = "u1"
+    password = "p1"
+    
+    form = LoginForm()
+
+    if flask.request.method == 'POST' and form.validate_on_submit() :
+        if form.username.data == username and form.password.data == password :
+            return(flask.redirect(flask.url_for('session')))
+
+    return flask.render_template('login.html', form=form)
