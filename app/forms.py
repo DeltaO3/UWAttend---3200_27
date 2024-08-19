7d3162c384a9bd518fe3566b3d8699af4e1f6c46
Adam <adam.wyatt@iinet.net.au>
2024-08-19 22:51:47 +0800

    merged changes with login page branch

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,12 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, SelectField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, ValidationError
+
+class LoginForm(FlaskForm):
+    username = StringField('Username:', validators=[DataRequired()])
+    password = PasswordField('Password:', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Log In')
 
 class SessionForm(FlaskForm):
     session_name = SelectField(
--- config.py
+++ config.py
@@ -1,2 +1,4 @@
-# Secret Key for Flask-WTF forms
-SECRET_KEY = '1e14cf8473e9b0b918f151a50c373200'
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
