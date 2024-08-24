e6d36b85dfa8b2dd5d85712dd44f1d441a5bb3fe
ozzy1323232 <168622926+ozzy1323232@users.noreply.github.com>
2024-08-24 13:05:39 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -6,32 +6,27 @@ from datetime import datetime
 # admin, this is the classes that are allocated to each staff member by the admin
 # the values here will be listed in /Sessions/ so a faculty can only select what they are
 # allocated
-
 #class administrator(db.Model):
 #   UnitName = 
 #   UnitCode =
 #   UserId   =
 #   what else is needed we can talk about at meetings?
 
-# AttendanceId will be populated with data from a google sheets file with the students information, until we get that file the studentId will be populated with the students name
-# the below values are automatically assigned their values by the database but we could change this, they wont need user input at the moment
-# DateTime          # defaults to CURRENT_TIMESTAMP
-# SignedOut         # defaults to CURRENT_TIMESTAMP
-# attendanceMarked  # defaults to "yes"
+# AttendanceId should be populated with data from a google sheets file with the students information, until we get that file the studentId will be populated with the students name
 class attendance_data(db.Model):
         
-    AttendanceId = db.Column(db.Text, primary_key = True)
-    UnitId = db.Column(db.Text)
-    StudentId = db.Column(db.Text)
-    Surname = db.Column(db.Text) 
-    Title = db.Column(db.Text)
-    PreferedName = db.Column(db.Text)
-    DateTime = db.Column(db.Text, datetime.now().strftime("%Y%m%d%H%M"))
-    SignedOut = db.Column(db.Text)
+    AttendanceId = db.Column(db.Text, primary_key = True)    # will be populated with data from the google sheets callista, atm its just the students name for testing purposes
+    UnitId = db.Column(db.Text) 
+    StudentId = db.Column(db.Text)     # callista provides this or user input
+    Surname = db.Column(db.Text)       # callista provides this
+    Title = db.Column(db.Text)         # callista provides this
+    PreferedName = db.Column(db.Text)  # callista provides this
+    DateTime = db.Column(db.Text, default=datetime.now().strftime("%Y%m%d%H%M"))
+    SignedOut = db.Column(db.Text)                           # empty until the sign all out button is pressed
     PeriodOfDay = db.Column(db.Text)
-    Consent = db.Column(db.Text)
-    AttendanceMarked = db.Column(db.Text)
-    SessionId = db.Column(db.Integer)        # this is a foreign key defined in the database itself
+    Consent = db.Column(db.Text, default="no")
+    AttendanceMarked = db.Column(db.Text, default="yes")     # defaults because its assumed when this function is used the attendance is getting marked
+    SessionId = db.Column(db.Integer)                        # this is a foreign key defined in the database itself
 
 # The Primary Key is a combination of (DateTime() + UnitCode)
 # UserId : this is who is running the class and configured the session
@@ -41,6 +36,7 @@ class session_data(db.Model):
     UserId = db.Column(db.Text)                # this is a foreign key defined in the database itself
     UnitName = db.Column(db.Text)
     UnitCode = db.Column(db.Text)
+    DateTime = db.Column(db.Text, default=datetime.now().strftime("%Y%m%d%H%M"))  # defaults to when session is created
 
 # renamed to login_data to avoid issues with other imported modules 
 class login_data(db.Model):
