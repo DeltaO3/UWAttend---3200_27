b2f00ecec99773f13da743f583db019584053d36
ozzy1323232 <168622926+ozzy1323232@users.noreply.github.com>
2024-08-24 13:17:20 +0800

    Update config.py
    added database file

--- config.py
+++ config.py
@@ -1,4 +1,7 @@
 import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'Attendance.db')
+    
