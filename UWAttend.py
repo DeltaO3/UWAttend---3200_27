635c6ff765bfe7897df2f8ff3060b487bc423987
Olivia Hanly (23331483) <23331483@student.uwa.edu.au>
2024-08-31 20:40:23 +0800

    Remove unnecessary imports from UWAttend.py

--- UWAttend.py
+++ UWAttend.py
@@ -1,8 +1,6 @@
-import sqlalchemy as sa
-import sqlalchemy.orm as so
 from app import app, db
 from app.models import User, Unit, Student, Session, Attendance
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Unit': Unit, 'Student': Student, 'Session': Session, 'Attendance': Attendance}
+    return {'db': db, 'User': User, 'Unit': Unit, 'Student': Student, 'Session': Session, 'Attendance': Attendance}
