c71546dd3c40a26bd946c2e5275c86ce8546f189
Olivia Hanly (23331483) <23331483@student.uwa.edu.au>
2024-08-31 18:06:36 +0800

    Add db init and shell context code

--- UWAttend.py
+++ UWAttend.py
@@ -1,1 +1,8 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import app, db
+from app.models import User, Unit, Student, Session, Attendance
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'sa': sa, 'so': so, 'db': db, 'User': User, 'Unit': Unit, 'Student': Student, 'Session': Session, 'Attendance': Attendance}
