dc6545b00156a93095b6c82368908058c50cec2d
Delta O <deltaoliver2@gmail.com>
2024-09-11 12:36:06 +0800

    Fixed typo causing crash

--- app/database.py
+++ app/database.py
@@ -140,7 +140,7 @@ def AddUnit(unitCode, unitName, studyPeriod, active, startDate, endDate, session
         db.session.add(UnitEntry)       # add the changes 
         db.session.commit()             # save the changes
 
-        return db.session.query(Unit).filter_by(unitcode=unitCode, startDate=startDate).first().unitID
+        return db.session.query(Unit).filter_by(unitCode=unitCode, startDate=startDate).first().unitID
     
     except IntegrityError as e:
         db.session.rollback()
