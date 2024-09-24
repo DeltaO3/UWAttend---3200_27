3d39902dfe3e3f1f341c273591775608fc08b77c
Olivia Hanly (23331483) <23331483@student.uwa.edu.au>
2024-09-24 09:56:14 +0800

    Update database.py and testdb.py with new schema

--- app/testdb.py
+++ app/testdb.py
@@ -5,25 +5,25 @@ from app.database import *
 with app.app_context():
 	print("initialising test database...")
 	#Assumes you initialise an admin with createadmin and env file, but will create an admin user here.
-	AddUser(12345678, "Admin", "Admin", "password", 1)
+	AddUser("admin@admin.com", "Admin", "Admin", "password", "admin")
 	#Admin creates unit:
-	unit1 = AddUnit("CITS3000", "Unit", "Sem1", 1, datetime(2024, 8, 12).date(), datetime(2024, 11, 10).date(), "Lab|Workshop", "Morning|Afternoon", 1, 1, 1, "nomark|suggest")
+	unit1 = AddUnit("CITS3000", "Computing 101", "Sem1",  datetime(2024, 8, 12).date(), datetime(2024, 11, 10).date(), "Lab|Workshop", "Morning|Afternoon", 1, 1, 1, "nomark|suggest")
 	#Assign facilitators to unit
-	AddUser(22224444, "Facilitator1", "Lastname", "facilitator1", 3)
-	AddUser(33335555, "Facilitator2", "Lastname", "facilitator2", 3)
-	AddUser(44446666, "Coordinator1", "Coordinate", "coordinate", 2)
+	AddUser("22224444@uwa.edu.au", "Facilitator1", "Lastname", "facilitator1", "facilitator")
+	AddUser("33335555@uwa.edu.au", "Facilitator2", "Lastname", "facilitator2", "facilitator")
+	AddUser("44446666@uwa.edu.au", "Coordinator1", "Coordinate", "coordinate", "facilitator")
 	#Add units to users
-	AddUnitToCoordinator(12345678, unit1)
-	AddUnitToFacilitator(12345678, unit1)
-	AddUnitToFacilitator(22224444, unit1)
-	AddUnitToFacilitator(33335555, unit1)
-	AddUnitToCoordinator(44446666, unit1)
-	AddUnitToFacilitator(44446666, unit1)
+	AddUnitToCoordinator("admin@admin.com", unit1)
+	AddUnitToFacilitator("admin@admin.com", unit1)
+	AddUnitToFacilitator("22224444@uwa.edu.au", unit1)
+	AddUnitToFacilitator("33335555@uwa.edu.au", unit1)
+	AddUnitToCoordinator("44446666@uwa.edu.au", unit1)
+	AddUnitToFacilitator("44446666@uwa.edu.au", unit1)
 	#Add sessions to unit
 	AddSession(unit1, "Lab", "Morning", datetime.now())
 	AddSession(unit1, "Workshop", "Morning", datetime.now())
 	#Add students to unit/sessions
-	AddStudent(55557777, "Alice", "Alicelast", "Ms", "Alice", unit1, 0)
-	AddStudent(66668888, "Robert", "Boblast", "Mr", "Bob", unit1, 0)
-	AddStudent(77777777, "Cathy", "Cathylast", "Ms", "Cathy", unit1, 1)
+	AddStudent("55557777", "Alice", "Alicelast", "Ms", "Alice", unit1, 0)
+	AddStudent("66668888", "Robert", "Boblast", "Mr", "Bob", unit1, 0)
+	AddStudent("77777777", "Cathy", "Cathylast", "Ms", "Cathy", unit1, 1)
 	print("successful!")
