561610770de81fdf083edea8ae225d022a3dfa94
ozzy1323232 <168622926+ozzy1323232@users.noreply.github.com>
2024-09-29 20:36:52 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -3,13 +3,27 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_apscheduler import APScheduler
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 login = LoginManager(app)
 login.login_view = '/login'
 
-from app import routes, models
+from app import routes, models, database
+
+#schedule
+scheduler = APScheduler()
+scheduler.init_app(app)
+scheduler.start()
+
+scheduler.add_job(
+    id='Scheduled Task',
+    func=database.delete_expired_units,  
+    trigger='interval',  
+    hours=24  
+)
