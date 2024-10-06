7cdff5f614946ba5f4e72d61a0eae8125d691385
ozzy1323232 <168622926+ozzy1323232@users.noreply.github.com>
2024-10-06 14:39:43 +0800

    sqlcipher

--- config.py
+++ config.py
@@ -1,8 +1,14 @@
+# config.py
 import os
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
 
+    # Use SQLCipher with the encryption key specified in the URI
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'app.db')
+        'sqlite+pysqlcipher://:password@/' + os.path.join(basedir, 'app.db') + \
+        '?cipher=aes-256-cfb&kdf_iter=64000'
+
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
