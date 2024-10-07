295bdde3b066d9d328c421d6d4ef62f0cd0d2932
Adam <adam.wyatt@iinet.net.au>
2024-10-07 21:41:57 +0800

    Database now encrypts with .env DATABASE_PASSWORD

--- config.py
+++ config.py
@@ -1,14 +1,18 @@
 # config.py
+from dotenv import load_dotenv
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
+# Load environment variables from the .env file
+load_dotenv(os.path.join(basedir, '.env'))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    DATABASE_PASSWORD = os.environ.get('DATABASE_PASSWORD')
 
     # Use SQLCipher with the encryption key specified in the URI
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite+pysqlcipher://:password@/' + os.path.join(basedir, 'app.db') + \
-        '?cipher=aes-256-cfb&kdf_iter=64000'
+        f'sqlite+pysqlcipher://:{DATABASE_PASSWORD}@/' + os.path.join(basedir, 'app.db')
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
