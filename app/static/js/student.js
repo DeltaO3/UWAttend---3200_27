80969d33bfadae4eed9da8a24053637d93193267
Delta O <deltaoliver2@gmail.com>
2024-10-07 14:08:49 +0800

    Getting basic rudimentary text detection of suggestions working

--- app/static/js/student.js
+++ app/static/js/student.js
@@ -1,29 +1,46 @@
-window.onload = function() {
+window.onload = function () {
     const comment_node = document.getElementById("comments")
     comment_node.textContent = comment_node.getAttribute("data-comment")
 
 }
 
 function removeStudent(id) {
-	// Submit the form
+    // Submit the form
     console.log("Student ID: " + id);
-    
+
     $("#removeStudentModal").modal('hide');
 
     window.location.href = '/remove_from_session?student_id=' + id
 
     // redirect to remove student with student information
 
-	return false; // Ensure no unexpected behaviour
+    return false; // Ensure no unexpected behaviour
 }
 
-$(document).ready(function() {
-    $('.suggestion').on('click', function() {
-        var suggestedText = $(this).data('text');
-        var commentsField = $('#comments'); 
+$(document).ready(function () {
+    const commentSuggestions = [];
+    $('.suggestion').each(function () {
+        commentSuggestions.push($(this).text())
+    })
 
-        commentsField.val(function(i, text) {
-            return text + (text ? ', ' : '') + suggestedText; 
+    $('.suggestion').on('click', function () {
+        var suggestedText = $(this).attr('id');
+        var commentsField = $('#comments');
+
+        commentsField.val(function (i, text) {
+            return text + (text ? ', ' : '') + suggestedText;
         });
     });
+
+
+
+    $('#comments').on("change keyup paste", function () {
+        let inputString = $('#comments').val();
+        console.log(inputString);
+        commentSuggestions.forEach(element => {
+            if (inputString.indexOf(element) >= 0) {
+                $('#' + element).fadeToggle("fast", "linear")
+            }
+        });
+    })
 });
