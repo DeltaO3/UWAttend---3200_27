41324c9890b4f990a9b092b413dfb5d7dac5f17c
Olivia Hanly (23331483) <23331483@student.uwa.edu.au>
2024-10-08 16:26:07 +0800

    Set modal route to updatesession or session depending on context

--- app/static/js/joinExistingSession.js
+++ app/static/js/joinExistingSession.js
@@ -11,17 +11,20 @@ function checkSessionExists() {
     $.ajax({
         type: "POST",
         url: "/checksessionexists",
-        data: $('form').serialize(),    // serialise the forms contents
+        data: $('form').serialize(),    // serialise the form's contents
         datatype: 'json',
         success: function(data) {
+            // if the session already exists, show the warning modal
             if (data['sessionExists'] === "true") {
                 let modalTextElement = $("#joinExistingSessionModalText").get(0)
 
                 const facilitatorNamesLength = data['facilitatorNames'].length
 
+                // if the session exists, but there are no students, say the session is empty
                 if (facilitatorNamesLength == 0) {
                     modalTextElement.innerHTML = "You are joining an existing empty session.";
                 }
+                // if the session exists and has students signed in, say which facilitators have signed them in
                 else {
                     modalTextElement.innerHTML = "You are joining an existing session with students signed in by: "
                     for (let i = 0; i < facilitatorNamesLength; i++) {
@@ -31,15 +34,15 @@ function checkSessionExists() {
                         }
                     }
                 }
-
                 $('#joinExistingSessionModal').modal('show');
             }
+            // if the session doesn't exist, immediately submit the session form as usual
             else if (data['sessionExists'] === "false") {
                 submitSessionForm();
             }
         },
         error: function(error) {
-            console.error("Error configuring session", error);
+            console.error("Error configuring/updating session", error);
         }
     });
 
@@ -48,17 +51,28 @@ function checkSessionExists() {
 
 function submitSessionForm() {
 
+    // hide the modal (if there is one)
     $('#joinExistingSessionModal').modal('hide');
+
+    // set route appropriately depending on whether form is to update or not
+    let route = '';
+    let update = $('form').attr('data-update');
+    if (update == "true") {
+        route = "/updatesession";
+    }
+    else {
+        route = "/session";
+    }
 
     $.ajax({
         type: "POST",
-        url: "/session",
+        url: route,
         data: $('form').serialize(),    // serialise the forms contents
         success: function() {
-            window.location.href = '/home';  // Redirect to home after successfully configuring session
+            window.location.href = '/home';  // Redirect to home after successfully configuring/updating session
         },
         error: function(error) {
-            console.error("Error configuring session", error);
+            console.error("Error configuring/updating session", error);
         }
     });
 
