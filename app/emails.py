b2d82344d6b3b20340dd61580596c1e05cee2943
ipr17 <23159504@student.uwa.edu.au>
2024-10-10 19:08:47 +0800

    changed formatting of welcome email template body html

--- app/emails.py
+++ app/emails.py
@@ -92,34 +92,34 @@ def get_welcome_email_details(recipient_encoded):
     <html>
     <head>
         <style>
-            body {
+            body {{
                 font-family: Arial, sans-serif;
                 margin: 0;
                 padding: 0;
                 background-color: #f4f4f4;
-            }
-            .container {
+            }}
+            .container {{
                 width: 100%;
                 padding: 20px;
-            }
-            .header {
+            }}
+            .header {{
                 background-color: #A9A9FF;
                 color: white;
                 padding: 10px 0;
                 text-align: center;
-            }
-            .content {
+            }}
+            .content {{
                 background-color: white;
                 padding: 20px;
                 border-radius: 5px;
                 box-shadow: 0 2px 5px rgba(0,0,0,0.2);
-            }
-            .footer {
+            }}
+            .footer {{
                 margin-top: 20px;
                 font-size: 12px;
                 text-align: center;
                 color: #888;
-            }
+            }}
         </style>
     </head>
     <body>
